e2b21d5a3ac4af11efb07c7f9461b597af9282ae
letterso <lettersony00@gmail.com>
2025-04-22 11:15:07 +0800

    adapt ceres2, change LocalParameterization to Manifold

--- ov_init/src/ceres/State_JPLQuatLocal.cpp
+++ ov_init/src/ceres/State_JPLQuatLocal.cpp
@@ -30,7 +30,7 @@ bool State_JPLQuatLocal::Plus(const double *x, const double *delta, double *x_pl
   // Apply the standard JPL update: q <-- [d_th/2; 1] (x) q
   Eigen::Map<const Eigen::Vector4d> q(x);
 
-  // Get delta into eigen
+  // Get delta into eigen 
   Eigen::Map<const Eigen::Vector3d> d_th(delta);
   Eigen::Matrix<double, 4, 1> d_q;
   double theta = d_th.norm();
@@ -48,7 +48,39 @@ bool State_JPLQuatLocal::Plus(const double *x, const double *delta, double *x_pl
   return true;
 }
 
-bool State_JPLQuatLocal::ComputeJacobian(const double *x, double *jacobian) const {
+bool State_JPLQuatLocal::PlusJacobian(const double *x, double *jacobian) const {
+  Eigen::Map<Eigen::Matrix<double, 4, 3, Eigen::RowMajor>> j(jacobian);
+  j.topRows<3>().setIdentity();
+  j.bottomRows<1>().setZero();
+  return true;
+}
+
+bool State_JPLQuatLocal::Minus(const double *y, const double *x, double *y_minus_x) const {
+
+  // Apply the standard JPL
+  Eigen::Map<const Eigen::Vector4d> q_y(y);
+  Eigen::Map<const Eigen::Vector4d> q_x(x);
+
+  // Do the update
+  Eigen::Map<Eigen::Vector3d> q_minus(y_minus_x);
+  Eigen::Matrix<double, 4, 1> q_minus_quat = ov_core::quat_multiply(q_y, ov_core::Inv(q_x));
+  // JPL -> 李代数
+  double qx = q_minus_quat(0), qy = q_minus_quat(1), qz = q_minus_quat(2), qw = q_minus_quat(3);
+  // 计算整体旋转角 θ = 2 * acos(qw)
+  double theta = 2.0 * std::acos(qw);
+  // 计算 sin(θ/2) 并判断退化
+  double sin_half = std::sqrt(1.0 - qw * qw);
+  Eigen::Vector3d axis;
+  if (sin_half < 1e-6) { 
+      axis = Eigen::Vector3d::UnitX();  // 默认返回x轴
+  } else {
+      axis = Eigen::Vector3d(qx, qy, qz) / sin_half;
+  }
+  q_minus = theta * axis;
+  return true;
+}
+
+bool State_JPLQuatLocal::MinusJacobian(const double *x, double *jacobian) const {
   Eigen::Map<Eigen::Matrix<double, 4, 3, Eigen::RowMajor>> j(jacobian);
   j.topRows<3>().setIdentity();
   j.bottomRows<1>().setZero();
